7f8878eb98520177c2d498543b6302d14f927c19
Orhan Sönmez <orhansonmez@gmail.com>
2019-11-09 15:14:34 +0100

    slugify + datetime formatting and arrival datetime

--- connections.py
+++ connections.py
@@ -1,7 +1,8 @@
 
 from requests_html import HTMLSession
+from slugify import slugify
 import argparse
-import datetime
+from datetime import datetime, timedelta
 import json
 import sys
 
@@ -13,11 +14,10 @@ def find_city_with_name(name):
     
     for id in cities:
         city = cities[id]
-        if(city['name'] == name):
+        if slugify(city['name'], separator='_') == slugify(name, separator='_'):
             return city
     
     return None
-
 
 with open('cities.json') as json_file:
     cities = json.load(json_file)
@@ -28,7 +28,7 @@ city2 = find_city_with_name(destination)
 if city1 is None or city2 is None:
     exit()
 
-departure_datetime = datetime.datetime.strptime(departure_date, "%Y-%m-%d")
+departure_datetime = datetime.strptime(departure_date, "%Y-%m-%d")
 
 search_url = "https://shop.global.flixbus.com/search?departureCity={}&arrivalCity={}&route={}-{}&rideDate={}"\
     .format(city1['id'], city2['id'], city1['name'], city2['name'], departure_datetime.strftime("%d.%m.%Y"))
@@ -43,10 +43,16 @@ for result in results:
     connection = {}
     
     connection['departure_station'] = result.xpath('//div[@class="station-name departure-station-name"]/text()')[0]
-    connection['departure_time'] = result.xpath('//div[@class="departure"]/text()')[0]
-
     connection['arrival_station'] = result.xpath('//div[@class="station-name arrival-station-name"]/text()')[0]
-    connection['arrival_time'] = result.xpath('//div[@class="arrival"]/text()')[0]
+
+    departure_time = result.xpath('//div[@class="departure"]/text()')[0]
+    departure_datetime = departure_datetime.replace(hour=int(departure_time.split(':')[0]), minute=int(departure_time.split(':')[1]))
+    connection['departure_datetime'] = departure_datetime.strftime("%Y-%m-%d %H:%M")
+
+    duration = result.xpath('//div[@class="col-xs-12 duration ride__duration ride__duration-messages"]/text()')[0].strip()
+    arrival_datetime = departure_datetime + timedelta(hours=int(duration.split(':')[0]),minutes=int(duration.split(':')[1]))
+    connection['arrival_datetime'] = arrival_datetime.strftime("%Y-%m-%d %H:%M")
+    # connection['arrival_time'] = result.xpath('//div[@class="arrival"]/text()')[0]
 
     euros = result.xpath('//span[@class="num currency-small-cents"]/text()')[0]    
     cents = result.xpath('//span[@class="num currency-small-cents"]/sup/text()')[0]
